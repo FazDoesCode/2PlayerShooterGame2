1039d2872d35866c8ba54f7a1a3a5252705d5b02
Arthur <shockwave1915@gmail.com>
2022-01-06 04:01:52 +0100

    0.0.1
    woo working on the game

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace TheGame
 {
@@ -8,6 +9,83 @@ namespace TheGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        int currentRes = 1;
+        int resScale = 1;
+        bool escapeKeyWasPressed = false;
+
+        //Declaring Backgrounds
+        Texture2D mainMenuSprite;
+        Texture2D controlsScreen;
+        
+        //Declaring Buttons
+        Texture2D oneButton;
+        Texture2D twoButton;
+        Texture2D coopButton;
+        Texture2D singleplayerButton;
+        Texture2D exitButton;
+        Texture2D settingsButton;
+
+        //Declaring Players
+        Texture2D playerOneSprite;
+        Texture2D playerOneDodgeSprite;
+        Texture2D playerTwoSprite;
+        Texture2D playerTwoDodgeSprite;
+        Texture2D graveStoneSprite;
+
+        // Main menu stuff
+        bool isInMainMenu = true;
+
+        // Position & walking stuff
+        double redTimeSinceLastWalked = 0;
+        double blueTimeSinceLastWalked = 0;
+        int redWalkSoundDelay = 400; // These are used to make walk sounds better
+        int blueWalkSoundDelay = 400; // They are kept seperate so they can speed up if the speed powerup is picked up
+
+        // Dodging stuff
+        int redDodgeDelay = 1200; // Red has 1.2 seconds of time between dodges, same as blue
+        int redInvulnTime = 500; // Only 500ms of invincibility
+        double redtimeSinceLastDodge = 0;
+        double redInvulnTimer = 0; // These are used in the dodge timer
+
+        int blueDodgeDelay = 1200; // All the same as red, however kept seperate so the dodge powerup can only effect the one who picked it up
+        int blueInvulnTime = 500;
+        double bluetimeSinceLastDodge = 0;
+        double blueInvulnTimer = 0;
+
+        // Firing Stuff
+        bool redfireDelay = false; // booleans used to determine whether or not red or blue can fire
+        bool redIsDodging = false; // This is used to stop dodge spam
+
+        bool bluefireDelay = false;
+        bool blueIsDodging = false;
+
+        // Declaring rectangles so I can use them in collisions
+        Rectangle redguyRect;
+        Rectangle blueguyRect;
+        Rectangle mainmenuRect;
+        Rectangle singleplayerButtonRect;
+        Rectangle coopButtonRect;
+        Rectangle settingsButtonRect;
+        Rectangle exitButtonRect;
+
+        // Red Guy Controls
+        Keys redguyMoveUp = Keys.W;
+        Keys redguyMoveDown = Keys.S;
+        Keys redguyMoveLeft = Keys.A;
+        Keys redguyMoveRight = Keys.D;
+        Keys redguyShoot = Keys.F;
+        Keys redguyDodge = Keys.G;
+
+        // Blue Guy Controls
+        Keys blueguyMoveUp = Keys.Up;
+        Keys blueguyMoveDown = Keys.Down;
+        Keys blueguyMoveLeft = Keys.Left;
+        Keys blueguyMoveRight = Keys.Right;
+        Keys blueguyShoot = Keys.OemPeriod;
+        Keys blueguyShoot2 = Keys.NumPad1;
+        Keys blueguyDodge = Keys.OemComma;
+        Keys blueguyDodge2 = Keys.NumPad2;
 
         public Game1()
         {
@@ -18,11 +96,30 @@ namespace TheGame
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             _graphics.IsFullScreen = false;
-            _graphics.PreferredBackBufferWidth = 1280;
-            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 480;
             _graphics.ApplyChanges();
+
+            //Backgrounds
+            mainMenuSprite = Content.Load<Texture2D>("Backgrounds/mainmenu");
+            controlsScreen = Content.Load<Texture2D>("Backgrounds/Controlsscreen");
+
+            //Buttons
+            oneButton = Content.Load<Texture2D>("Items/1xbutton");
+            twoButton = Content.Load<Texture2D>("Items/2xbutton");
+            coopButton = Content.Load<Texture2D>("Items/coopbutton");
+            exitButton = Content.Load<Texture2D>("Items/exitbutton");
+            singleplayerButton = Content.Load<Texture2D>("Items/singleplayerbutton");
+            settingsButton = Content.Load<Texture2D>("Items/settingsbutton");
+
+            //Players
+            playerOneSprite = Content.Load<Texture2D>("Players/Redguy");
+            playerTwoSprite = Content.Load<Texture2D>("Players/Blueguy");
+            playerOneDodgeSprite = Content.Load<Texture2D>("Players/Redguydodgelarge");
+            playerTwoDodgeSprite = Content.Load<Texture2D>("Players/Blueguydodgelarge");
+            graveStoneSprite = Content.Load<Texture2D>("Players/Gravestone");            
+
             base.Initialize();
         }
 
@@ -30,15 +127,53 @@ namespace TheGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             // TODO: Add your update logic here
+            MouseState mouseState = Mouse.GetState();
+
+            if (mouseState.RightButton == ButtonState.Pressed)
+            {
+                Debug.WriteLine(mouseState.X + "," + mouseState.Y);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.F4))
+            {
+                if (currentRes == 1 && escapeKeyWasPressed == false)
+                {
+                    escapeKeyWasPressed = true;
+                    resScale = 2;
+                    _graphics.PreferredBackBufferWidth = 1600;
+                    _graphics.PreferredBackBufferHeight = 960;
+                    _graphics.ApplyChanges();
+                    currentRes = 2;
+                }
+                else if (currentRes == 2 && escapeKeyWasPressed == false)
+                {
+                    escapeKeyWasPressed = true;
+                    resScale = 1;
+                    _graphics.PreferredBackBufferWidth = 800;
+                    _graphics.PreferredBackBufferHeight = 480;
+                    _graphics.ApplyChanges();
+                    currentRes = 1;
+                }
+            }
+            if (Keyboard.GetState().IsKeyUp(Keys.F4))
+            {
+                escapeKeyWasPressed = false;
+            }
+
+            if(isInMainMenu == true)
+            {
+                if(mouseState.X > 290 * resScale && mouseState.X < 290 * resScale + oneButton.Width * resScale && mouseState.Y > 200 * resScale && mouseState.Y > oneButton.Height* resScale && mouseState.LeftButton == ButtonState.Pressed)
+                {
+                    Debug.WriteLine("poopoo");
+                }
+            }
+
             base.Update(gameTime);
         }
 
@@ -46,7 +181,27 @@ namespace TheGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin(SpriteSortMode.Deferred,
+                BlendState.AlphaBlend,
+                SamplerState.PointClamp
+               );
+
+            Rectangle mainmenuRect = new Rectangle(0, 0, mainMenuSprite.Width * resScale, mainMenuSprite.Height * resScale);
+            Rectangle singleplayerButtonRect = new Rectangle(290 * resScale, 200 * resScale, oneButton.Width * resScale, oneButton.Height * resScale);
+            Rectangle coopButtonRect = new Rectangle(290 * resScale, 280 * resScale, oneButton.Width * resScale, oneButton.Height * resScale);
+            Rectangle settingsButtonRect = new Rectangle(5 * resScale, 400 * resScale, oneButton.Width * resScale, oneButton.Height * resScale);
+            Rectangle exitButtonRect = new Rectangle(595 * resScale, 400 * resScale, oneButton.Width * resScale, oneButton.Height * resScale);
+
+            if (isInMainMenu == true)
+            {
+                _spriteBatch.Draw(mainMenuSprite, mainmenuRect, Color.White);
+                _spriteBatch.Draw(singleplayerButton, singleplayerButtonRect, Color.White);
+                _spriteBatch.Draw(coopButton, coopButtonRect, Color.White);
+                _spriteBatch.Draw(settingsButton, settingsButtonRect, Color.White);
+                _spriteBatch.Draw(exitButton, exitButtonRect, Color.White);
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
